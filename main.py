ad65046b920cb7e8ebad46014e032ac2f35d11f0
melvinlim <dxlim@outlook.com>
2024-04-10 18:06:11 -0700

    removed comments

--- main.py
+++ main.py
@@ -8,14 +8,11 @@ maxRespLen=200
 startTok='<|im_start|>'
 endTok='<|im_end|>'
 
-messages=startTok+'eve\nhi alice.'+endTok+'\n'
 dolphin_url='http://192.168.68.107:8080'
 capybara_url='http://192.168.68.107:8090'
 airoboros_url='http://192.168.68.107:8070'
 
-#dolphin=LlamaModel(dolphin_url,'alice','you are alice.  you\'re role playing being a wizard at hogwarts with your friends bob and eve.  respond with ... to allow someone else to speak.')
-#capybara=LlamaModel(capybara_url,'bob','you are bob.  you\'re role playing being a wizard at hogwarts with your friends alice and eve.  respond with ... to allow someone else to speak.')
-#airoboros=LlamaModel(airoboros_url,'eve','you are eve.  you\'re role playing being a wizard at hogwarts with your friends alice and bob.  respond with ... to allow someone else to speak.')
+messages=startTok+'eve\nhi alice.'+endTok+'\n'
 
 aliceText="you are alice.  a college student.  you're chatting with your friends bob and eve at a coffee shop during spring break."
 bobText="you are bob.  a college student.  you're chatting with your friends alice and eve at a coffee shop during spring break."
@@ -40,5 +37,3 @@ for i in range(chatrounds):
 	#messages=chatOnce(capybara,messages)
 	#messages=chatOnce(airoboros,messages)
 
-#print()
-#print(result)
