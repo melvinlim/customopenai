653a23b32ec684a85ac4294320b985f07fa4adf7
melvinlim <dxlim@outlook.com>
2024-05-13 20:04:41 -0700

    added stuff

---
+++ selfchat.py
@@ -0,0 +1,26 @@
+from customopenai import LlmModel
+
+STREAM=True
+
+maxRespLen=200
+
+#assistant_url='http://192.168.68.107:8080'
+assistant_url='http://192.168.68.106:8080'
+
+assistantText="you're an AI girl. the year is 2024."
+
+document=""""""
+
+#messages='<|im_start|>'+document+'<|im_end|>'+'\n'
+#messages=document+'\n'
+#messages="<|im_start|>human:what year is it?<|im_end|>\n"
+messages=""
+
+assistant=LlmModel(assistant_url,'girl',assistantText,stream=STREAM,maxRespLen=maxRespLen,top_p=0.1)
+msg=assistant.chatresp(messages)
+
+for i in range(0):
+	#messages+='<|im_start|>girl:'+msg+'<|im_end|>'
+	messages+='<|im_start|>girl:'+msg+'<|im_end|><|im_start|>silence...<|im_end>'
+	msg=assistant.chatresp(messages)
+
